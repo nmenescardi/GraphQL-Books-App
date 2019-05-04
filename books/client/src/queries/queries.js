885be9e3ead444a8e6ac0d6c1134ef490e2cc182
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-05-04 10:35:55 -0500

    add queries params

--- books/client/src/queries/queries.js
+++ books/client/src/queries/queries.js
@@ -19,11 +19,10 @@ const getBooksQuery = gql`
 `;
 
 const addBookMutation = gql`
-  mutation {
-    addBook(name: "", genre: "", authorId: "") {
+  mutation($name: String!, $genre: String!, $authorId: ID!) {
+    addBook(name: $name, genre: $genre, authorId: $authorId) {
       id
       name
-      authorId
     }
   }
 `;
