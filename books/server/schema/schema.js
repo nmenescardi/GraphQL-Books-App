d9927644ed1cf6eb9b89593bec8f99f7f30549ed
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-05-04 01:15:03 -0500

    add lodash - add graphiql

--- books/server/schema/schema.js
+++ books/server/schema/schema.js
@@ -1,11 +1,19 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+
+//Dummy data for lazy ones :)
+const books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
@@ -18,10 +26,14 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: {
         id: {
-          type: GraphQLString
+          type: GraphQLID
         }
       },
-      resolve(parent, args) {}
+      resolve(parent, args) {
+        return _.find(books, {
+          id: args.id
+        });
+      }
     }
   }
 });
