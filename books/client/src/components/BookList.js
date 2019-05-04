4f397820fed4e7463cb96dc514f0d2beaf41c533
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-05-04 09:53:52 -0500

    add cors - fetch books

--- books/client/src/components/BookList.js
+++ books/client/src/components/BookList.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+//import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
 
 export class BookList extends Component {
-  static propTypes = {};
-
+  displayBooks() {
+    const { loading, books } = this.props.data;
+    if (loading) {
+      return <div>Loading Books...</div>;
+    } else {
+      return books.map(book => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  }
   render() {
+    console.log('this.props', this.props);
     return (
       <div>
-        <ul id="book-lis">
-          <li>Book Name</li>
-        </ul>
+        <ul id="book-lis">{this.displayBooks()}</ul>
       </div>
     );
   }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
--- books/server/app.js
+++ books/server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(
   'mongodb+srv://root:1234@cluster0-rmy7e.mongodb.net/test?retryWrites=true'
